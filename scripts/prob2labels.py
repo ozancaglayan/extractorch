4404c45aa947d053ffdb815651974d2d2d786da1
Ozan Caglayan <ozancag@gmail.com>
2018-10-09 17:15:23 +0200

    scripts: update

--- scripts/prob2labels.py
+++ scripts/prob2labels.py
@@ -12,12 +12,32 @@ def load_cat_file(fname):
     return cats
 
 
+def load_inoutdoor(fname):
+    d = {}
+    with open(fname) as f:
+        for line in f:
+            category, env = line.strip().split()
+            # originally: indoor 1, outdoor 2
+            # modified: indoor 1, outdoor -1
+            d[category[3:]] = 1 if env == '1' else -1
+    return d
+
+
+def detect_env(class_map, labels, pred_scores):
+    weighted_score = sum([
+        class_map[lbl] * sc for lbl, sc in zip(labels, pred_scores)]) / len(labels)
+
+    return '[INDOOR]' if weighted_score > 0 else '[OUTDOOR]'
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='prob2npy')
     parser.add_argument('-i', '--input-npy', type=str, required=True,
                         help='Path to prob features file .npy.')
     parser.add_argument('-c', '--labels', type=str, required=True,
                         help='Path to labels file.')
+    parser.add_argument('-p', '--places-io', type=str, default=None,
+                        help='Path to places indoor/outdoor labels file.')
     parser.add_argument('-s', '--segment-index', type=str, default='',
                         help='Optional segment index file to concat to output.')
     parser.add_argument('-k', '--topk', type=int, default=10,
@@ -33,23 +53,39 @@ if __name__ == '__main__':
 
     if args.segment_index:
         segment_index = load_cat_file(args.segment_index)
+        assert len(segment_index) == feats.shape[0], \
+            "segment index line count does not match feats."
 
     assert feats.shape[1] == len(cats), ".npy file feature dim != # of labels"
 
-    idxs = np.argpartition(-feats, kth=args.topk, axis=-1)[:, :args.topk]
+    inout = None
+    if args.places_io:
+        inout = load_inoutdoor(args.places_io)
+        assert len(inout) == feats.shape[1], \
+            "indoor/outdoor file not compatible with feats."
 
+    idxs = np.argpartition(-feats, kth=args.topk, axis=-1)[:, :args.topk]
     lines = []
 
     for i in range(idxs.shape[0]):
         scores = feats[i, idxs[i]]
         labels = [cats[j] for j in idxs[i]]
+
+        # Add indoor/outdoor detection output
+        inout_result = detect_env(inout, labels, scores) if inout else ['N/A']
+
         ordered = (sorted(zip(scores, labels), key=lambda x: x[0], reverse=True))
 
-        lines.append(' '.join(['{} ({:.4f})'.format(l, s) for (s, l) in ordered]))
+        # right-hand side, i.e. labels and their scores
+        line = ' '.join(['{} ({:.4f})'.format(l, s) for (s, l) in ordered])
 
+        # Add indoor/outdoor
+        lines.append('{} {}'.format(inout_result, line))
+
+    # Add segment IDs
     if segment_index:
-        lines = ['{} {}'.format(seg, line) for seg, line in zip(segment_index, lines)]
+        lines = ['{:15s} {}'.format(seg, line) for seg, line in zip(segment_index, lines)]
 
+    # Dump
     for line in lines:
         print(line)
-
